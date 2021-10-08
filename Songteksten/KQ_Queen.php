ce1fd05f21562422f84601ebc3ed65aba5c5bcb5
pvdk31 <pvdk.7@outlook.com>
2021-10-08 15:02:26 +0200

    Songtekst Bohemian Rhapsody

---
+++ Songteksten/KQ_Queen.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Killer Queen</title>
+</head>
+    <body>
+        <div class="ContainerSongtekst">
+            <div class="Blauw">
+                <p>
+                    "Bohemian Rhapsody"<br>
+                    <br>
+                    Is this the real life?<br>
+                    Is this just fantasy?<br>
+                    Caught in a landslide<br>
+                    No escape from reality<br>
+                    <br>
+                    Open your eyes<br>
+                    Look up to the skies and see<br>
+                    I'm just a poor boy, I need no sympathy<br>
+                    Because I'm easy come, easy go<br>
+                    Little high, little low<br>
+                    Any way the wind blows<br>
+                    Doesn't really matter to me, to me<br>
+                    <br>
+                    Mama, just killed a man<br>
+                    Put a gun against his head<br>
+                    Pulled my trigger, now he's dead<br>
+                    Mama, life had just begun<br>
+                    But now I've gone and thrown it all away<br>
+                    <br>
+                    Mama, ooh<br>
+                    Didn't mean to make you cry<br>
+                    If I'm not back again this time tomorrow<br>
+                    Carry on, carry on as if nothing really matters<br>
+                    <br>
+                    Too late, my time has come<br>
+                    Sends shivers down my spine<br>
+                    Body's aching all the time<br>
+                    Goodbye, everybody, I've got to go<br>
+                    Gotta leave you all behind and face the truth<br>
+                    <br>
+                    Mama, ooh (Any way the wind blows)<br>
+                    I don't want to die<br>
+                    I sometimes wish I'd never been born at all<br>
+                    <br>
+                    I see a little silhouetto of a man<br>
+                    Scaramouche, Scaramouche, will you do the Fandango?<br>
+                    Thunderbolt and lightning very, very frightening me<br>
+                    (Galileo) Galileo<br>
+                    (Galileo) Galileo<br>
+                    Galileo Figaro<br>
+                    Magnifico-o-o-o-o<br>
+                    <br>
+                    I'm just a poor boy, nobody loves me<br>
+                    He's just a poor boy from a poor family<br>
+                    Spare him his life from this monstrosity<br>
+                    <br>
+                    Easy come, easy go, will you let me go?<br>
+                    Bismillah! No, we will not let you go (Let him go!)<br>
+                    Bismillah! We will not let you go (Let him go!)<br>
+                    Bismillah! We will not let you go (Let me go!)<br>
+                    Will not let you go (Let me go!)<br>
+                    Never let you go (Never, never, never, never let me go)<br>
+                    Oh oh oh oh<br>
+                    No, no, no, no, no, no, no<br>
+                    Oh, mama mia, mama mia (Mama mia, let me go)<br>
+                    Beelzebub has a devil put aside for me, for me, for me<br>
+                    <br>
+                    So you think you can stone me and spit in my eye?<br>
+                    So you think you can love me and leave me to die?<br>
+                    Oh, baby, can't do this to me, baby<br>
+                    Just gotta get out, just gotta get right outta here<br>
+                    <br>
+                    Ooooh, ooh yeah, ooh yeah<br>
+                    <br>
+                    Nothing really matters<br>
+                    Anyone can see<br>
+                    Nothing really matters<br>
+                    Nothing really matters to me<br>
+                    <br>
+                    Any way the wind blows...
+                </p>
+            </div>
+         </div>
+    </body>
+</html>
