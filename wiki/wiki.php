c15fc94c45bf38edaf3c5f46eba0883487510827
TjKalau <71122246+TjKalau@users.noreply.github.com>
2021-10-08 14:44:38 +0200

    button toegevoegd

--- wiki/wiki.php
+++ wiki/wiki.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>wiki</title>
 </head>
 <body>
-    
+    <div class="button_wiki">
+        <button>test</button>
+        <button>klik</button>
+        <button>klok</button>
+        <button>glock</button>
+    </div>
 </body>
 </html>
