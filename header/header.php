854d08066665ff40601c6ea614af650dfb0dd4d2
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2021-10-08 14:38:07 +0200

    header.php

---
+++ header/header.php
@@ -0,0 +1,19 @@
+<?php require_once 'backend/config.php'; ?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="<?php echo $base_url; ?>/css/style.css">
+<!-- <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet"> -->
+
+<header>
+    <div class="container">
+        <nav>
+            <img src="<?php echo $base_url; ?>/img/logo-big-v4.png" alt="logo">
+            <div class="links">
+                <a href="<?php echo $base_url; ?>/index.php">Home</a> 
+                <a href="<?php echo $base_url; ?>/scrappy.html">Takenoverzicht</a>
+                <a href="<?php echo $base_url; ?>/login.php">Login</a>
+            </div>
+        </nav>
+    </div>
+</header>
