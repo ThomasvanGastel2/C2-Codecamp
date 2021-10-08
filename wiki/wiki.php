39468bffc3a05caa0bd675eea486a180a6c10ad9
TjKalau <71122246+TjKalau@users.noreply.github.com>
2021-10-08 14:57:05 +0200

    styling button

--- wiki/wiki.php
+++ wiki/wiki.php
@@ -9,10 +9,22 @@
 </head>
 <body>
     <div class="button_wiki">
-        <button>test</button>
-        <button>klik</button>
-        <button>klok</button>
-        <button>glock</button>
+        <div class="boeken">
+            <button>test</button>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas nesciunt corporis eligendi, maxime et culpa voluptatem aut qui aperiam rem quae. Consequatur temporibus exercitationem sed officiis nemo vero enim?</p>
+        </div>
+        <div class="cirkelzagen">
+            <button>klik</button>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero atque, quos sapiente accusamus ducimus quia sint similique excepturi? Omnis ab explicabo esse nam ducimus ut, vero distinctio dolore maxime commodi.</p>
+        </div>
+        <div class="kettingzagen">
+            <button>klok</button>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi fugiat in at vitae officiis deleniti. Voluptatibus veritatis similique est ipsum ab architecto maiores nulla, fuga quod asperiores. Eos, vitae accusamus?</p>
+        </div>
+        <div class="hamers">
+            <button>glock</button>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, veritatis consectetur! Voluptatem quam suscipit accusantium quae rem asperiores explicabo, consectetur ut odio inventore officia culpa saepe? Nobis sed tempora mollitia.</p>
+        </div>
     </div>
 </body>
 </html>
