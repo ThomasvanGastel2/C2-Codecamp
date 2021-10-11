6f6fe821599a3f289e26d17192b005a32fd9bc24
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2021-10-11 09:22:15 +0200

    header.php update

--- header/header.php
+++ header/header.php
@@ -2,8 +2,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo $base_url; ?>/css/style.css">
-<!-- <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet"> -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
 
 <header>
     <div class="container">
