fd5b95d4773f9dd815baa7c08740d9ac070df318
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2021-10-12 12:51:08 +0200

    Header.php

--- header/header.php
+++ header/header.php
@@ -11,18 +11,29 @@
             <img src="<?php echo $base_url; ?>/img/aubergine.png" alt="logo">
             <div class="links">
                 <a href="<?php echo $base_url; ?>/index.php">Home</a> 
-                <a href="<?php echo $base_url; ?>/wiki/wiki.php">Wiki</a>
                 <div class="dropdown">
                 <button
                 class="dropbtn">Songteksten</button>
                     <div class="dropdown-content">
-                    <a href="<?php echo $base_url; ?>/Songteksten/BR_Queen.php">Bohemian Rapsody</a> 
-                    <a href="<?php echo $base_url; ?>/Songteksten/KQ_Queen.php">Killer Queen </a> 
-                    <a href="<?php echo $base_url; ?>/Songteksten/Madness_Muse.php">Madness</a>
-                    <a href="<?php echo $base_url; ?>/Songteksten/BR_Queen.php"></a>  
+                        <a href="<?php echo $base_url; ?>/Songteksten/BR_Queen.php">Bohemian Rapsody</a> 
+                        <a href="<?php echo $base_url; ?>/Songteksten/KQ_Queen.php">Killer Queen </a> 
+                        <a href="<?php echo $base_url; ?>/Songteksten/Madness_Muse.php">Madness</a>
+                        <a href="<?php echo $base_url; ?>/Songteksten/Uprising_MyBaby.php">Uprising (MyBaby)</a>  
+                        <a href="<?php echo $base_url; ?>/Songteksten/Uprising_Muse.php">Uprising (Muse)</a>
+                        <a href="<?php echo $base_url; ?>/Songteksten/PIB_Muse.php">Plug in baby</a>  
+                        <a href="<?php echo $base_url; ?>/Songteksten/Transylvania_IronMaiden.php">Transylvia</a>  
                     </div>
                 </div>
-                <a href="<?php echo $base_url; ?>/Films/
+                <div class="dropdown">
+                <button
+                class="dropbtn">Wiki</button>
+                    <div class="dropdown-content">
+                        <a href="<?php echo $base_url; ?>/wiki/boeken.php">Boeken</a> 
+                        <a href="<?php echo $base_url; ?>/wiki/cirkelzagen.php">Cirkelzagen </a> 
+                        <a href="<?php echo $base_url; ?>/wiki/hamers.php">Hamers</a>
+                        <a href="<?php echo $base_url; ?>/wiki/kettingzagen.php">Kettingzagen</a>   
+                    </div>
+                </div>
             </div>
         </nav>
     </div>
