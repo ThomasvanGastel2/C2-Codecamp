d0634933d13baecc9ad034d63ad906ca18cb509d
TjKalau <71122246+TjKalau@users.noreply.github.com>
2021-10-12 14:14:46 +0200

    favicon op alle pagina's

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="shortcut icon" href="img/favicon.png" type="image">  
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,5 +12,20 @@
     <?php
     require_once 'header.php'
     ?>
+    <div class="content">
+
+<div class="text">
+
+    <h1><span id="datetime"></span></h1>
+
+    <script>
+
+        var dt = new Date();
+
+        document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+
+    </script>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam ea nobis adipisci nemo perspiciatis doloremque illo! Porro eligendi veniam, perferendis ipsa alias, impedit dolores voluptatem, fugiat omnis maiores itaque.</p>
 </body>
 </html>
