b8991be919f32a2e70a5415401205d38714a922f
pvdk31 <pvdk.7@outlook.com>
2021-10-14 14:16:41 +0200

    James Bond pagina af
    De James Bond pagina is nu af

--- jamesBond/jamesbond.php
+++ jamesBond/jamesbond.php
@@ -8,8 +8,31 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    require_once '../header.php'
-    ?>
+    <header>
+        <?php
+        require_once '../header.php'
+        ?>
+    </header>
+    <h1>Alle James Bond films met Roger Moore</h1>
+    <h3>"Live and Let Die" – 1973</h3>
+    <p>Live and Let Die is de achtste James Bondfilm, geproduceerd door EON Productions. De film werd uitgebracht in 1973 en is gebaseerd op het gelijknamige boek van Ian Fleming uit 1954. Het is de eerste Bondfilm waarin Roger Moore de rol van Bond vertolkte, ter vervanging van Sean Connery. Voodoo en Tarot spelen een belangrijke rol in de film. </p>    
+    <h3>"The Man with the Golden Gun" – 1974</h3>
+    <p>The Man with the Golden Gun is de negende James Bondfilm geproduceerd door EON Productions, met Roger Moore als James Bond. De film is uitgebracht in 1974. Hij is gebaseerd op het gelijknamige boek uit 1965. De film speelt zich af tegen de achtergrond van de oliecrisis van 1973. </p>
+    <h3>"The Spy Who Loved Me" – 1977</h3>
+    <p>The Spy Who Loved Me is de tiende James Bondfilm geproduceerd door EON Productions, met Roger Moore als James Bond. De film is uitgebracht in 1977. Het gelijknamige boek uit 1962 werd echter door veel lezers niet zo gewaardeerd, waardoor Fleming alleen toestemming gaf voor het gebruik van de titel in de film. </p>
+    <h3>"Moonraker" – 1979</h3>
+    <p>Moonraker is het derde James Bondboek geschreven in 1955 door Ian Fleming. In 1979 werd het boek verfilmd door EON Productions. De rol van James Bond werd vertolkt door Roger Moore en de film werd geregisseerd door Lewis Gilbert. Omdat sciencefictionfilms zeer succesvol bleken te zijn, ging Bond in deze film naar de ruimte terwijl in het originele boek Bond gewoon op de aarde blijft.</p>
+    <h3>"For Your Eyes Only" – 1981</h3>
+    <p>For Your Eyes Only is de twaalfde James Bondfilm geproduceerd door EON Productions, met Roger Moore als James Bond. De film is uitgebracht in 1981. Het is gebaseerd op een kort verhaaltje uit de gelijknamige verhalenbundel uit 1960.
+    De titelsong werd gezongen door Sheena Easton. Het nummer For Your Eyes Only kwam in Nederland op nummer 1 in de Nederlandse Top 40. </p>
+    <h3>"Octopussy" – 1983</h3>
+    <p>Octopussy is de dertiende James Bondfilm geproduceerd door EON Productions, met voor de zesde keer Roger Moore als James Bond.
+
+De film kwam in hetzelfde jaar uit als de onofficiële (niet van EON Productions) Bondfilm Never Say Never Again met Sean Connery als James Bond. Connery speelde eerder al in een aantal officiële Bondfilms, wat leidde tot een felle concurrentiestrijd.
+
+De film kwam voor het eerst uit op 6 juni 1983 en werd tussen 10 augustus 1982 en 25 januari 1983 op verschillende locaties opgenomen. Het budget werd geschat op 27,5 miljoen dollar. De film verdiende zichzelf ruim terug: hij bracht wereldwijd bijna 190 miljoen dollar op. Roger Moore ontving 4 miljoen dollar voor zijn werk aan deze film. </p>
+    <h3>"A View to a Kill" – 1985</h3>
+    <p>A View to a Kill (1985) is de veertiende film in de James Bondserie, geregisseerd door John Glen. Het was de zevende en laatste film waarin de rol van agent 007 werd gespeeld door Roger Moore. </p>
+
 </body>
 </html>
