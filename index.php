8905bc1b9236be44c48b10e8b9f582a15b1da7db
TjKalau <71122246+TjKalau@users.noreply.github.com>
2021-10-18 09:30:58 +0200

    style css main

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Team Aubergine</title>
 </head>
 <body>
     <?php
@@ -14,25 +14,29 @@
     ?>
     <div class="content">
 
-<div class="text">
+    <div class="text">
+        <h1><span id="datetime"></span></h1>
+        <h1><span id="time"></span></h1>
+    </div>
 
-    <h1><span id="datetime"></span></h1>
-    <h1><span id="time"></span></h1>
+    <div class="datetime-box">
+        <div class="date">
+            <script>
+                var dt = new Date();
+                document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+            </script>
+        </div>
 
-
-    <script>
-
-        var dt = new Date();
-
-        document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
-
-    </script>
-
-    <script>
-        var dt = new Date();
-        document.getElementById("time").innerHTML = dt.toLocaleTimeString();
-    </script>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam ea nobis adipisci nemo perspiciatis doloremque illo! Porro eligendi veniam, perferendis ipsa alias, impedit dolores voluptatem, fugiat omnis maiores itaque.</p>
+        <div class="timedisplay">
+            <script>
+                var dt = new Date();
+                document.getElementById("time").innerHTML = dt.toLocaleTimeString();
+            </script>
+            <p>Team Aubergine</p>
+            <p>Telefoonnummer: 0612589462</p>
+            <p></p>
+            <p></p>
+        </div>
+    </div>
 </body>
 </html>
