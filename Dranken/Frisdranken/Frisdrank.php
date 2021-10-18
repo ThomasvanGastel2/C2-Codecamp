ce5a97a7f3dd86afbc5d10a2e8fd69990069f675
pvdk31 <pvdk.7@outlook.com>
2021-10-18 09:43:30 +0200

    Dranken

---
+++ Dranken/Frisdranken/Frisdrank.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="../img/favicon.png" type="image">  
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frisdrank</title>
+</head>
+<body>
+    <?php
+
+        require_once  '../../header.php'
+?>
+    <h3>Frisdrank</h3>
+    <p>
+        Frisdrank is het in 1956 door reclamemaker Dick Schiferli bedachte woord voor koolzuurhoudende limonade. Het wordt ook wel priklimonade of prik genoemd. In ruimere zin worden, volgens de Nederlandse Warenwet, ook niet-koolzuurhoudende limonades, sportdranken en energiedranken tot de frisdranken gerekend.
+        Een typische frisdrank bestaat uit koolzuurhoudend water, waaraan suiker, aroma's (of echte vruchtenextracten) en eventueel andere hulpstoffen zijn toegevoegd.
+        Frisdrank behoort tot de rode zone van de voedingsdriehoek (buiten de voedingsdriehoek).
+        In het Surinaams-Nederlands wordt deze drank 'soft' genoemd, afgeleid van het Engelse softdrink.
+        <br>
+    </p>
+
+    <h3>Coca-Cola</h3>
+    <p>
+    Coca-Cola is een merk cola. Het is een suikerhoudende koolzuurhoudende frisdrank die wordt verhandeld door The Coca-Cola Company. Het is sinds 1893 als handelsmerk geregistreerd. Inmiddels zijn het logo, de flessen en de drank samen een wereldwijd bekend icoon voor de Amerikaanse levensstijl. 
+    <br>
+    </p>
+    <div class="george_clooney_button">
+        <p>Externe links:</p>
+        <button type="button"    
+        onclick="window.location.href='http://localhost/C2-Codecamp/information/Roger_Moore.php'" />>Roger Moore</button>
+    </div>
+
+    <h3>7 up</h3>
+    <p>
+    7Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group in de Verenigde Staten en PepsiCo in de rest van de wereld. 
+    </p>
+    <br>
+
+    <h3>Sinas</h3>
+    <p>
+    Sinas is in Nederland de benaming voor de doorgaans gele of oranje, meestal koolzuurhoudende frisdrank met sinaasappelsmaak. Zoals de meeste frisdranken bevat sinas veel suiker of in de light-versie kunstmatige zoetstoffen. Het gehalte aan sinaasappelsap bedraagt ten minste 4%. Bekende merken zijn Fanta (van de Coca-Cola Company) en Sisi (Vrumona); Orangina onderscheidt zich doordat er werkelijk vruchtvlees in zit.     <br>
+    </p>
+
+    
+</body>
+</html>
