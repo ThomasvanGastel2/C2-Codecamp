584ed7a817361feb55c20ed1485ae30e123b9894
pvdk31 <pvdk.7@outlook.com>
2021-10-22 14:42:00 +0200

    header fix

--- header.php
+++ header.php
@@ -8,20 +8,20 @@
 <header>
     <div class="container">
         <nav>
-            <img src="<?php echo $base_url; ?>/img/logo_codecamp.png" alt="logo">
+            <img src="<?php echo $base_url; ?>/img/transparentBG.png" alt="logo">
             <div class="links">
                 <a href="<?php echo $base_url; ?>/index.php">Home</a> 
                 <div class="dropdown">
                 <button
                 class="dropbtn">Songteksten</button>
                     <div class="dropdown-content">
-                        <a href="<?php echo $base_url; ?>/Songteksten/BR_Queen.php">Bohemian Rapsody</a> 
-                        <a href="<?php echo $base_url; ?>/Songteksten/KQ_Queen.php">Killer Queen </a> 
-                        <a href="<?php echo $base_url; ?>/Songteksten/Madness_Muse.php">Madness</a>
-                        <a href="<?php echo $base_url; ?>/Songteksten/Uprising_MyBaby.php">Uprising (MyBaby)</a>  
-                        <a href="<?php echo $base_url; ?>/Songteksten/Uprising_Muse.php">Uprising (Muse)</a>
-                        <a href="<?php echo $base_url; ?>/Songteksten/PIB_Muse.php">Plug in baby</a>  
-                        <a href="<?php echo $base_url; ?>/Songteksten/WATC_Queen.php ">We are the champions</a>
+                        <a href="<?php echo $base_url; ?>/Songteksten/br_queen.php">Bohemian Rapsody</a> 
+                        <a href="<?php echo $base_url; ?>/Songteksten/kq_queen.php">Killer Queen </a> 
+                        <a href="<?php echo $base_url; ?>/Songteksten/madness_muse.php">Madness</a>
+                        <a href="<?php echo $base_url; ?>/Songteksten/uprising_mybaby.php">Uprising (MyBaby)</a>  
+                        <a href="<?php echo $base_url; ?>/Songteksten/uprising_muse.php">Uprising (Muse)</a>
+                        <a href="<?php echo $base_url; ?>/Songteksten/pib_muse.php">Plug in baby</a>  
+                        <a href="<?php echo $base_url; ?>/Songteksten/watc_queen.php ">We are the champions</a>
                     </div>
                 </div>
                 <div class="dropdown">
@@ -31,8 +31,8 @@
                         <a href="<?php echo $base_url; ?>/wiki/boeken.php">Boeken</a> 
                         <a href="<?php echo $base_url; ?>/wiki/Gereedschap/Zagen/cirkelzagen.php">Cirkelzagen </a> 
                         <a href="<?php echo $base_url; ?>/wiki/Gereedschap/hamers.php">Hamers</a>
-                        <a href="<?php echo $base_url; ?>/wiki/Gereedschap/Schroevendraaiers.php">Schroevendraaiers</a>   
-                        <a href="<?php echo $base_url; ?>/wiki/Gereedschap/Zagen/Handzagen.php">Handzagen</a> 
+                        <a href="<?php echo $base_url; ?>/wiki/Gereedschap/schroevendraaiers.php">Schroevendraaiers</a>   
+                        <a href="<?php echo $base_url; ?>/wiki/Gereedschap/Zagen/handzagen.php">Handzagen</a> 
                         <a href="<?php echo $base_url; ?>/wiki/koffie.php">Koffie</a>     
                     </div>
                 </div>
@@ -45,8 +45,7 @@
                         <a href="<?php echo $base_url; ?>/information/timotydalto.php">Timoty Dalto</a> 
                         <a href="<?php echo $base_url; ?>/information/lukeskywalker.php">Luke Skywalker</a>
                         <a href="<?php echo $base_url; ?>/information/jamesbond.php">James Bond</a>
-                        <a href="<?php echo $base_url; ?>/information/Roger_Moore.php">Roger Moore</a>
-                        <a href="<?php echo $base_url; ?>/ExternalLinks.php">External Links</a>
+                        <a href="<?php echo $base_url; ?>/information/roger_moore.php">Roger Moore</a>
 
                     </div>
                 </div>
@@ -54,8 +53,8 @@
                 <button
                 class="dropbtn">Dranken</button>
                     <div class="dropdown-content">
-                        <a href="<?php echo $base_url; ?>/dranken/Warme Dranken/Warmdrank.php">Warme dranken</a> 
-                        <a href="<?php echo $base_url; ?>/dranken/Frisdranken/Frisdrank.php">Frisdrank</a> 
+                        <a href="<?php echo $base_url; ?>/dranken/Warme Dranken/warmdrank.php">Warme dranken</a> 
+                        <a href="<?php echo $base_url; ?>/dranken/Frisdranken/frisdrank.php">Frisdrank</a> 
                     </div>
                 </div>
             </div>
