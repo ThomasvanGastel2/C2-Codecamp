dc6b3638403efd20c04e163e4a7feb217b622d30
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2021-11-02 13:53:20 +0100

    styling index.php

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
     ?>
 
 <div class="content">
-    <div class="digital-clock"id = "digital-clock"> </div>
-    <p>Actuele tijd</p>
-    <p class="date"id="date"></p>
-    <p>Actuele datum</p>
+    <div class="clock">
+        <div class="digital-clock"id = "digital-clock"></div>
+        <p>Actuele tijd</p>
+    </div>
+    <div class="actual-date">
+        <p class="date"id="date"></p>
+        <p>Actuele datum</p>
+    </div>
     <script src = "script.js"> </script>
 </div>
     <?php
