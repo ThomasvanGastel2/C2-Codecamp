b7077ae067747cb0f3ca6b9a5192b22d20a57cde
TjKalau <71122246+TjKalau@users.noreply.github.com>
2021-11-02 14:23:42 +0100

    boeken page

---
+++ boeken/hitchhikers.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="../img/favicon.png" type="image">  
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dr No</title>
+</head>
+<body>
+    <?php
+    require_once '../header.php'
+    ?>
+<div class="drno">
+    <h1>Dr No</h1>
+        <p>Darth Vader is een personage uit de Star Wars-films. Hij is als Sith-leerling ondergeschikt aan keizer Palpatine, ook bekend als Darth Sidious.
+        In de jaren 1977, 1980 en 1983 werd Darth Vader gespeeld door acteur David Prowse en stuntman Bob Anderson. Zijn stem werd echter ingesproken door James Earl Jones, omdat diens stem veel zwaarder, duisterder en dreigender klonk.</p>
+        <br>
+        <!-- <img src="../img/darthvader.png" alt="dartvader"> -->
+
+    <h2>Acteurs</h2>
+    <p>In de eerste drie opgenomen films (Episodes IV, V en VI) speelt David Prowse de rol van Darth Vader (behalve in enkele gevechtsscènes, waar Bob Anderson hem speelt). De stem wordt gedubd door de acteur James Earl Jones. In deze films is het gezicht van Darth Vader slechts twee keer te zien. Vlak voordat Darth Vader overlijdt, in Episode VI, wordt zijn masker afgedaan en wordt hij gespeeld door Sebastian Shaw. Later is hij te zien als geest. In de oorspronkelijke versie is Shaw hier opnieuw te zien, maar in de opnieuw uitgebrachte dvd-versie is deze geestverschijning aangepast, zodat Hayden Christensen te zien is. Hayden speelde Anakin Skywalker in de prequeltrilogie (Episodes I, II en III), het personage wat aan het eind van Episode III transformeert in Darth Vader.
+    Daarnaast wordt Darth Vader ook nog door andere acteurs gespeeld in een aantal videospellen. In een hoorspel uit de jaren 80 en 90 werd zijn stem door Brock Peters ingesproken. Voor verschillende videospellen werd zijn stem ingesproken door Matt Sloan en later door Scott Lawrence</p>
+    <br>
+
+    <h2>Trivia</h2>
+    <ul>
+        <li>De keversoort Agathidium vaderi is vernoemd naar Darth Vader.</li>
+        <li>De haftsoort Darthus vadorus is vernoemd naar Darth Vader.</li>
+        <li>Darth Vader wordt in veel media geparodieerd, bijvoorbeeld als Dark Helmet in de film Spaceballs, als Dark Laser in de serie Fairly Odd Parents en als jongere broer in de online-serie Chad Vader - Day Shift Manager.</li>
+        <li>In Episode IV is Darth Vader op geen enkel hemellichaam aanwezig; hij bevindt zich enkel binnen ruimtevaartuigen.</li>
+        <li>Professioneel darter John Part gebruikt The Imperial March als opkomstnummer tijdens dartwedstrijden. Z'n bijnaam Darth Maple is ook gebaseerd op de naam Darth Vader.</li>
+        <li>Darth Vaders kostuum is gebaseerd op dat van een Japanse samoerai. Een andere inspiratiebron was Heinrich Himmler, die eveneens in zwart gekleed ging.</li>
+        <li>Tijdens de burgeroorlog in de Oekraïne transformeerden inwoners van Odessa een standbeeld van Lenin in Darth Vader.</li>
+    </ul>
+</div>
+<?php
+
+    require_once  '../footer.php'
+    ?>
+
+
+</body>
+</html>
